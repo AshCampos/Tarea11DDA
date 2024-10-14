aafc3dd22915dbef329c1c0d696377481dcbf2d3
AshCampos <125852862+AshCampos@users.noreply.github.com>
2024-10-13 22:52:52 -0600

    FibonacciConDiccionario.py

--- FibonacciConDiccionario.py
+++ FibonacciConDiccionario.py
@@ -1,3 +1,5 @@
+#Programa que hace la serie de fibonacci teniendo como entrada n la posición de la serie y de salida el enesimo número de la serie, usando diccionarios
+
 def fibonacciConDic(n, fib_dict={1: 1, 2: 1}):
     if n in fib_dict:  
         return fib_dict[n]  
